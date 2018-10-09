58c90c4be324516c0357d656d34fbbef0d80a947
overdodactyl <patjohns1216@gmail.com>
2018-10-08 20:24:50 -0600

    initial upload after re-structure

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
+var exec = require('gulp-exec');
+
+/* Minify all base code, edit in place */
+gulp.task('minify_base_code', function() {
+  return gulp.src(['css/*.css'])
+    .pipe(cleanCSS({
+      level : 2 ,
+      format: 'beautify'
+    }))
+    .pipe(gulp.dest(function(file) {
+      return file.base;
+    }));
+});
+
+
+/* Add everything to userChrome */
+gulp.task('userChrome', function() {
+  return gulp.src(['css/color_variables.css', 'css/[^color_variables]*.css'])
+    .pipe(concatCss('userChrome.css'))
+    .pipe(gulp.dest('.'));
+});
+
+/* Minify final user files */
+gulp.task('minify_final', function() {
+  return gulp.src(['userChrome.css'])
+    .pipe(cleanCSS({
+      level : 2 ,
+      format: 'beautify'
+    }))
+    .pipe(gulp.dest(function(file) {
+      return file.base;
+    }));
+});
+
+
+/* All */
+gulp.task('all', gulp.parallel('userChrome', function() {
+  return gulp.src(['userChrome.css'])
+    .pipe(cleanCSS({
+      level : 1 ,
+      format: 'beautify'
+    }))
+    .pipe(gulp.dest('.'));
+}));
+
+/* Publish */
+gulp.task('publish', gulp.series('minify_base_code', 'userChrome', 'minify_final'));
+
+
+
+/* Gulp Push - used to push to GitHub and re-add internal UUIDs */
+gulp.task('push', function() {
+  return gulp.src('.')
+    .pipe(exec('git push'))
+});
